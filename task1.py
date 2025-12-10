aa8371974476fbf9f0cf2bcd915517ef7c9d1c85
maeflowermeadows <maeflowermeadows@gmail.com>
2025-12-10 21:04:52 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiplication)
+print("division:", division)
+
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+lnam = (input("Enter your last name: "))
+fnam = (input("Enter your first name: "))
+print("Hello! we're so excited to have you on board", fnam+" "+lnam)
